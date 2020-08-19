34b91b2ec329e5f988b1eed35f2928a9d0db0420
Hugo Delgado <hugo.delgado1@outlook.com>
2020-08-19 07:43:48 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,29 +2,35 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
+        name: "Lord Montague", age: 53
         #your key/value pairs here
         },
       :matriarch => {
+        name: "Lady Montague", age: 54
         #your key/value pairs here
         },
       :hero => {
+        name: "Romero", age: 15, status: "Alive"
         #your key/value pairs here
         },
-      :hero_friends => []
-   }, 
+      :hero_friends => ["Benvolio", "Mercutio"]
+   },
    :capulet => {
       :patriarch => {
+        name: "Lord Capulet", age: 50
         #your key/value pairs here
         },
       :matriarch => {
+        name: "Lady Capulet" age: 51
         #your key/value pairs here
         },
       :heroine => {
+        name: "Juliet", age: 15, status: "Alive"
         #your key/value pairs here
         },
-      :heroine_friends => []
+      :heroine_friends => ["Steven", "Nurse"]
    }
   }
 
-  
+
 end
